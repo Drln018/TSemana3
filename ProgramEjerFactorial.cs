99434af73756bae031eb51133e8a051fc93cdaba
Drln018 <79181952+Drln018@users.noreply.github.com>
2021-02-26 13:23:42 -0600

    Add files via upload

---
+++ ProgramEjerFactorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EJERFACTORIAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+                Console.Write("Escriba un numero ");
+                n= Convert.ToInt32(Console.ReadLine());
+            
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("El Factorial de " + n + " es: " + fact);
+
+            Console.ReadKey();
+        } 
+    } 
+}
---
+++ ProgramSeriel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EJERSERIEFACTORIAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, j, i, auxiliar;
+            Console.WriteLine("Ingrese numero ");
+            j = Convert.ToInt32(Console.ReadLine());
+            a = 0;
+            b = 1;
+            for (i = 0; i < j; i++)
+            {
+                auxiliar = a;
+                a = b;
+                b = auxiliar + a;
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+    }
+}
